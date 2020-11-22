30a3ea206604de65ee968fb8f73f8bb72e46528d
haratakayasu09060071672@gmail.com <Haratakayasu09060071672@gmail.com>
2020-11-22 18:01:21 +0900

    add mongo db model and routers in the backend in order to establish the connection with MongoDB

--- amazon-mimic-backend/data.js
+++ amazon-mimic-backend/data.js
@@ -1,5 +1,21 @@
+import bcrypt from 'bcryptjs'
+
 
 const data = {
+  users:[
+    {
+      name: 'Takayasu',
+      email: 'admin@example.com',
+      password:bcrypt.hashSync('1234',8),
+      isAdmin:true,
+    },
+    {
+      name: 'John',
+      email: 'user@example.com',
+      password:bcrypt.hashSync('1234',8),
+      isAdmin:false,
+    }
+  ],
   products: [
     {
       _id: '1',
